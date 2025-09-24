d34ed9e30ec60ec82482f9ae4bbb07c735aa5c70
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-09-25 00:19:31 +0200

    feat: Mobile apps setup

---
+++ android/app/src/main/java/io/libertai/chat/MainActivity.java
@@ -0,0 +1,5 @@
+package io.libertai.chat;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.libertai.chat',
+  appName: 'LibertAI',
+  webDir: 'dist'
+};
+
+export default config;
