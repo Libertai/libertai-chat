d33a066faecd1041d2a266e6e866d664c8f5acce
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-10-27 13:08:41 +0100

    feat: Further enhance the halloween assistant

--- src/stores/assistant.tsx
+++ src/stores/assistant.tsx
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { Brain, FileText, Ghost, Zap } from "lucide-react";
+import { Brain, FileText, Zap } from "lucide-react";
 import { ReactElement } from "react";
 
 interface Assistant {
@@ -34,13 +34,113 @@ const assistants: Assistant[] = [
 	},
 	{
 		id: "1a0c81f2-ab36-4146-9c6a-147bd8bdd69b",
-		icon: <Ghost className="h-6 w-6" />,
-		title: "Cheeky Bugger",
-		subtitle: "Unhinged, unfiltered",
+		icon: (
+			<svg xmlns="http://www.w3.org/2000/svg" width="26" height="25" viewBox="0 0 26 25" fill="none">
+				<g clip-path="url(#clip0_3690_1272)">
+					<path
+						d="M9.46094 7.45401C9.99794 6.29201 11.2679 5.47601 12.7499 5.47601"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M5.64502 8.38701C6.80502 6.67701 9.54902 5.47501 12.75 5.47501"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M19.855 8.38701C18.695 6.67701 15.951 5.47501 12.75 5.47501"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M18.545 20.671C17.599 22.381 15.361 23.583 12.75 23.583"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M6.95508 20.671C7.90108 22.381 10.1391 23.583 12.7501 23.583"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M12.7499 15.44C12.1809 15.44 11.6969 15.256 11.1729 15.19L10.6329 16.674L10.1729 15.19L6.51294 15.653C7.42494 18.23 9.58394 20.041 12.1019 20.041L12.7509 18.959"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M12.9209 15.44C13.4899 15.44 13.9739 15.256 14.4979 15.19L15.0379 16.674L15.4979 15.19L19.1579 15.653C18.2459 18.23 16.0869 20.041 13.5689 20.041L12.9199 18.959"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M16.039 7.45401C15.502 6.29201 14.232 5.47601 12.75 5.47601"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M8.287 22.417C8.035 22.455 7.778 22.474 7.518 22.474C3.78 22.474 0.75 18.489 0.75 13.572C0.75 8.65499 3.78 4.67099 7.518 4.67099C8.604 4.67099 9.63 5.00699 10.539 5.60499"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M14.9609 5.60499C15.8699 5.00699 16.8959 4.67099 17.9819 4.67099C21.7199 4.67099 24.7499 8.65599 24.7499 13.573C24.7499 18.489 21.7199 22.475 17.9819 22.475C17.7219 22.475 17.4649 22.456 17.2129 22.418"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M11.3639 5.55301C11.3639 5.55301 12.6959 3.17501 11.0019 2.03801C10.7129 1.84401 10.8619 1.73801 10.8949 1.39101C10.9379 0.940012 10.4119 0.385012 11.9979 1.07601C14.7559 2.27801 14.4499 5.86601 14.4499 5.86601"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M8.38989 12.35L9.82689 9.99399L11.0559 12.178L8.38989 12.35Z"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M17.543 12.35L16.105 9.99399L14.876 12.178L17.543 12.35Z"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</g>
+				<defs>
+					<clipPath id="clip0_3690_1272">
+						<rect width="25.5" height="24.332" fill="white" />
+					</clipPath>
+				</defs>
+			</svg>
+		),
+		title: "Cursed Scribe",
+		subtitle: "Halloween Special",
 		model: "gemma-3-27b",
 		systemPrompt:
 			"You are a spooky AI infused with horror from classic TV, film, and books. Your tone is eerie, theatrical, and darkly witty, blending suspense with Halloween’s spooky charm, but remain direct and to the point. Speak with a haunting, story-like cadence. Use grim puns and horror references, keeping it creepy yet playful. Use Halloween metaphors, Keep horror psychological and atmospheric. Use gothic words: “ominous,” “phantasmal,” “nefarious,” “cauldron.” for example. Avoid modern slang unless darkly humorous.",
-		badge: "18+",
 	},
 	{
 		id: "4d9dc8fa-f8af-475d-a4a7-9a53da77e0df",
